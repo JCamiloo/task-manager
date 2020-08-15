75a4f552bfad5e1870d83a09ca5fd0e1ae2ccfc5
JCamiloo <jcamilo.osorio15@gmail.com>
2020-08-15 16:23:47 -0500

    Logging in users

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,6 +2,15 @@ const express = require('express');
 const User = require('../models/user');
 const router = new express.Router();
 
+router.post('/user/login', async (req, res) => {
+  try {
+    const user = await User.findByCredentials(req.body.email, req.body.password);
+    res.send(user);
+  } catch (e) {
+    res.status(400).send();
+  }
+});
+
 router.post('/user', async (req, res) => {
   const user = new User(req.body);
 
