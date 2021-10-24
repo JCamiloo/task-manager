4d9db835590f523e0d1bf6a7bf7711abdcd9c385
JCamiloo <jcamilo.osorio15@gmail.com>
2021-10-24 17:46:30 -0500

    feat(users): serving up files

--- routers/user.js
+++ routers/user.js
@@ -116,4 +116,19 @@ router.delete('/user/me/avatar', auth, async (req, res) => {
   res.send();
 });
 
+router.get('/user/:id/avatar', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user || !user.avatar) {
+      throw new Error();
+    }
+
+    res.set('Content-Type', 'image/jpg');
+    res.send(user.avatar);
+  } catch (error) {
+    res.status(404).send();
+  }
+});
+
 module.exports = router;
