62a67adbc0426405e3abf74da8b3a16bbf8f7b79
JCamiloo <jcamilo.osorio15@gmail.com>
2021-10-24 18:25:36 -0500

    feat(users): auto cropping and image formatting

--- routers/user.js
+++ routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const multer = require('multer');
+const sharp = require('sharp');
 const User = require('../models/user');
 const auth = require('../middleware/auth');
 
@@ -103,7 +104,13 @@ router.delete('/user/me', auth, async (req, res) => {
 });
 
 router.post('/user/me/avatar', auth, upload.single('avatar'), async (req, res) => {
-  req.user.avatar = req.file.buffer;
+  const buffer = await sharp(req.file.buffer)
+    .resize({ width: 250, height: 250 })
+    .png()
+    .toBuffer();
+  
+  req.user.avatar = buffer;
+
   await req.user.save();
   res.send();
 }, (error, req, res, next) => {
@@ -111,6 +118,10 @@ router.post('/user/me/avatar', auth, upload.single('avatar'), async (req, res) =
 });
 
 router.delete('/user/me/avatar', auth, async (req, res) => {
+  if (!req.user.avatar) {
+    return res.status(404).send();
+  }
+
   req.user.avatar = undefined;
   await req.user.save();
   res.send();
@@ -124,7 +135,7 @@ router.get('/user/:id/avatar', async (req, res) => {
       throw new Error();
     }
 
-    res.set('Content-Type', 'image/jpg');
+    res.set('Content-Type', 'image/png');
     res.send(user.avatar);
   } catch (error) {
     res.status(404).send();
